5a7dd454ff0fa6736188b82e5f4be55b1230908a
Gamerman2001 <Tro12185@gmail.com>
2020-10-10 01:57:25 -0400

    added hero and background

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
 import Nav from './Components/Nav/Nav'
+import Hero from './Components/Hero/Hero'
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Hero />
     </div>
   );
 }
---
+++ src/Components/Hero/Hero.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Hero.scss";
+
+export default function Hero() {
+   return (
+      <div className="hero-section">
+         <div className="container">
+            <div className="hero-wrapper">
+              <h1>Brewing Coffee Since 1959</h1>
+            </div>
+         </div>
+      </div>
+   );
+}
